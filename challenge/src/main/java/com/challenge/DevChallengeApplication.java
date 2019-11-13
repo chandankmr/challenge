66d462cc590e8732cad5cbf42d4d64a6a2451169
chandankmr <chandankmrmca@gmail.com>
2019-11-13 11:59:12 +0530

    test

---
+++ challenge/src/main/java/com/challenge/DevChallengeApplication.java
@@ -0,0 +1,14 @@
+package com.challenge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevChallengeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevChallengeApplication.class, args);
+		System.out.println("testing");
+	}
+
+}
