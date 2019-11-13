903139fe99eadccb322898b04760219bdca60c2e
chandankmr <chandankmrmca@gmail.com>
2019-11-13 12:26:14 +0530

    removed sout

--- challenge/src/main/java/com/challenge/DevChallengeApplication.java
+++ challenge/src/main/java/com/challenge/DevChallengeApplication.java
@@ -8,7 +8,6 @@ public class DevChallengeApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DevChallengeApplication.class, args);
-		System.out.println("testing");
 	}
 
 }
